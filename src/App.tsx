69935b8c88b578731400f4412e91a1f04a72e5ea
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-11-11 19:39:08 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
