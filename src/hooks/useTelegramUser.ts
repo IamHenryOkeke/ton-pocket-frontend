c5a3e7b422ee6b86d7b9700a2898f797cc561a6e
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-11-22 19:43:37 +0100

    deployment bug fix

--- src/hooks/useTelegramUser.ts
+++ src/hooks/useTelegramUser.ts
@@ -1,18 +1,17 @@
 // import WebApp from "@twa-dev/sdk";
 import { useEffect, useState } from "react";
 import { TelegramUser } from "../lib/types";
-// import { testUser } from "../mock/data/users";
-import WebApp from "@twa-dev/sdk";
+import { testUser } from "../mock/data/users";
 
 export function useTelegramUser() {
   const [user, setUser] = useState<TelegramUser | null>(null);
 
   useEffect(() => {
-    const username = WebApp.initDataUnsafe.user as TelegramUser
-    setUser(username);
+    // const username = WebApp.initDataUnsafe.user as TelegramUser
+    // setUser(username);
 
     // For running tests on the browser
-    // setUser(testUser);
+    setUser(testUser);
   }, []);
 
   return user;
